94c4210e9d41ba7f101f383dd639b9878a956a22
NIdeaForNickname <lazavicbagdan@gmail.com>
2025-08-11 21:25:19 +0300

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
+import { createPortal } from "react-dom";
 import "./App.css"
+
+const Modal = ({isOpen, onClose, modalContainer}) => {
+    if (!isOpen || !modalContainer) return null;
+
+    return createPortal(
+        <div className="modal-overlay">
+            <div className="modal-content">
+                <h2 className="modal-title">Limit reached</h2>
+                <p className="modal-text">Multiplication table size should not exceed 20</p>
+                <button className="modal-button" onClick={onClose}>
+                    Close
+                </button>
+            </div>
+        </div>,
+        modalContainer
+    )
+}
 
 const createRow = ({num, row}) => {
     return (
@@ -13,7 +31,8 @@ const createRow = ({num, row}) => {
     )
 }
 
-const createTable = ({num}) => {
+function CreateTable({num}) {
+    console.log(num)
     return (
         <>
             {Array.from({length: num}, (id, ind) => {return ind}).map(i => (
@@ -25,9 +44,21 @@ const createTable = ({num}) => {
     )
 }
 
-
 export default function App() {
-    const [size, setSize] = useState(5); // Default size
+    const [size, setSize] = useState(5);
+    const [showPortal, setShowPortal] = useState(false);
+    const [modalContainer, setModalContainer] = useState(null);
+
+    useEffect(() => {
+        const div = document.createElement("div");
+        div.id = "modal-root"
+        document.body.appendChild(div);
+        setModalContainer(div);
+
+        return () => {
+            document.body.removeChild(div);
+        }
+    }, [])
 
     return (
         <div>
@@ -37,13 +68,24 @@ export default function App() {
                     type="number"
                     value={size}
                     min="1"
-                    onChange={(e) => setSize(Number(e.target.value))}
+                    onChange={(e) => {
+                        const newVal = Number(e.target.value);
+                        if (newVal > 20) setShowPortal(true);
+                        else setSize(newVal);
+                    }}
                 />
+                <Modal
+                    isOpen={showPortal}
+                    onClose ={() => setShowPortal(false)}
+                    modalContainer={modalContainer}
+                />
+
             </label>
+
 
             <table>
                 <tbody>
-                    {createTable({num: size})}
+                    <CreateTable num={size} />
                 </tbody>
             </table>
         </div>
